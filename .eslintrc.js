fea3bdc7f3ca6cabc766bdfa3d04bab480ad2980
Vertigo <vertigomaill@gmail.com>
2021-09-22 22:50:44 +0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base/legacy"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
