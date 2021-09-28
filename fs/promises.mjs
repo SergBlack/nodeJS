e09c88cfd9a5c8cb9aa2d1c07e821bcd0f55def8
Vertigo <vertigomaill@gmail.com>
2021-09-28 23:36:01 +0300

    promises 2

--- fs/promises.mjs
+++ fs/promises.mjs
@@ -2,6 +2,8 @@ import fs from "fs";
 
 const { promises } = fs;
 const path = "./fs/test.txt";
+const path2 = "./fs/test2.txt";
+const path3 = "./fs/test3.txt";
 
 promises.readFile(path, "utf-8")
   .then((fileContent) => console.log(fileContent))
@@ -19,3 +21,25 @@ const read = async () => {
 read()
   .then((fileContent) => console.log(fileContent))
   .catch((err) => console.error(err));
+
+// eslint-disable-next-line max-len
+const readFile = (filename, encoding) => new Promise((res, rej) => fs.readFile(filename, encoding, (err, data) => {
+  if (err) {
+    rej(err);
+  } else {
+    res(data.toString());
+  }
+}));
+
+readFile(path, "utf8")
+  .then(data => {
+    console.log(data);
+    return readFile(path2, "utf8");
+  })
+  .then(data => {
+    console.log(data);
+    return readFile(path3, "utf8");
+  })
+  .then(data => {
+    console.log(data);
+  });
